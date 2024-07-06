bdf1c6475bd3a9c5274c583878c0be87bb378342
GitGitro <108683123+GitGitro@users.noreply.github.com>
2024-07-06 19:37:08 +0200

    Remove libs file, edit licenses, edit gitignore

--- app/src/main/java/website/leifs/delta/breezyweather/DeltaMoonDrawable.kt
+++ app/src/main/java/website/leifs/delta/breezyweather/DeltaMoonDrawable.kt
@@ -1,17 +1,17 @@
 /**
- * This file is part of Breezy Weather Pixel Icon Provider.
+ * This file is part of Breezy Weather Delta Icon Provider.
  *
- * Breezy Weather Pixel Icon Provider is free software: you can redistribute it and/or modify it
+ * Breezy Weather Delta Icon Provider is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, version 3 of the License.
  *
- * Breezy Weather Pixel Icon Provider is distributed in the hope that it will be useful, but
+ * Breezy Weather Delta Icon Provider is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Breezy Weather Pixel Icon Provider. If not, see <https://www.gnu.org/licenses/>.
+ * along with Breezy Weather Delta Icon Provider. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package website.leifs.delta.breezyweather
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.6.0-alpha08" apply false
+    kotlin("android") version "2.0.0" apply false
 }
