e6d84fb6a56e098c2d5f2735c4f3f4d6b3f9f4a9
GitGitro <108683123+GitGitro@users.noreply.github.com>
2024-07-13 15:41:43 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "website.leifs.delta.breezyweather"
         minSdk = 11
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
     }
 
